a9ca6839706669df08057b101e9c87c37404acd5
Leu Family <romanianfobjr@gmail.com>
2021-09-13 15:12:52 -0500

    server cookie privacy set up with express-session and connect-session-knex

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-/**
+/*
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
   This is achieved by setting 'saveUninitialized' to false, and by not
@@ -13,13 +13,35 @@ const cors = require("cors");
 
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
- */
+*/
+
+const session = require('express-session')
+const store = require('connect-session-knex')(session)
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'noone', // this belongs in the environment
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: false, // in dev, this should be true
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new store({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+}))
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
